f1356c859bee9904ed9b510aaf6c5329f71ec814
Dante <dante@thisislanguage.com>
2014-07-26 16:52:50 +0100

    многоразовое использование модулей

--- js/static_only/driver.js
+++ js/static_only/driver.js
@@ -15,13 +15,27 @@ var driver = {
 				driver.go_to_page($(el).attr('data-page'));
 			});
 		});
-		
-		$('.site_map a').first().click();
 	},
 	go_to_page: function(page_name) {
 		$('body').empty();
 		var template = templates[page_name+'_page_tpl'];
-		var rendered = Mustache.render(template, $.extend(text[page_name+'_page'], data[page_name+'_page']));
+		var page_text = text[page_name+'_page'];
+		var page_data = data[page_name+'_page'];
+		var all_text = [page_text, page_data];
+		var page_partials = partials[page_name+'_page'];
+		var combined_partials = {};
+		for (var i = 0; i < page_partials.length; i++) {
+			var partial_template = templates[page_partials[i]+'_tpl'];
+			var partial_text = text[page_partials[i]];
+			var partial_data = data[page_partials[i]];
+			all_text.push(partial_text, partial_data);
+			combined_partials[page_partials[i]] = partial_template;
+		}
+		all_text.push(data.common, text.common);
+		all_text.unshift({});
+		var combined_text = $.extend.apply(this, all_text);
+		console.log(combined_text);
+		var rendered = Mustache.render(template, combined_text, combined_partials);
 		$('body').html(rendered);
 	}
 }
